7cc4f58e33db3a0c2a52e97c574d82d3b751a068
Andre Vargas <vargasandre1@hotmail.com>
2018-08-22 19:17:41 -0300

    Usar Typeahead para pesquisar por usuários e redirecionar para seus perfis.

---
+++ client/Cabecalho/Pesquisa.js
@@ -0,0 +1,20 @@
+Template.Pesquisa.rendered = function() {
+  Meteor.typeahead.inject();
+}
+
+Template.Pesquisa.helpers({
+  items: function() {
+    // data source function
+    // TODO fetch items from meteor collection
+    return Meteor.users.find().fetch().map(function(object){
+      return {id: object._id, value: object.profile.name};
+    });
+  },
+  selected: function(event, suggestion, datasetName) {
+    // event - the jQuery event object
+    // suggestion - the suggestion object
+    // datasetName - the name of the dataset the suggestion belongs to
+    // TODO your event handler here
+    FlowRouter.go("/perfis/" + suggestion.id);
+  }
+});
